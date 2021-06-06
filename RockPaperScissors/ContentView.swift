8b9ca8bdefb55bd619f6690db2c9d4284e25b1f1
luke.inger@jcb-finance.co.uk <luke.inger@jcb-finance.co.uk>
2021-06-06 17:44:31 +0100

    Add initial gameplay element

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var actions = ["Rock", "Paper", "Scissors"]
+    
+    @State private var shouldWin = Bool.random()
+    @State private var score:Int = 0
+    @State private var appChoice:Int = Int.random(in: 0..<3)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Score: \(score)")
+            Text("App Move: \(self.actions[appChoice])")
+            Text("Outcome: \(shouldWin ? "Win" : "Lose")")
+            HStack{
+                ForEach(0..<actions.count){ number in
+                    Button(action:{
+                        buttonTapped(number)
+                    })
+                    {
+                        Text("\(self.actions[number])")
+                            .padding()
+                            .background(Color.orange)
+                            .foregroundColor(Color.black)
+                            .clipShape(Capsule())
+                    }
+                }
+            }
+        }
+    }
+    
+    func buttonTapped(_ action:Int){
+        
+        var hasWon:Bool=false
+        
+        switch action {
+        case 0:
+            if (appChoice == 2){
+                hasWon = true
+            }
+        case 1:
+            if (appChoice == 0){
+                hasWon = true
+            }
+        default:
+            if (appChoice == 1){
+                hasWon = true
+            }
+        }
+        
+        if (shouldWin && hasWon){
+            self.score += 1
+        } else {
+            self.score += 1
+        }
+        
+        newRound()
+    }
+    
+    func newRound(){
+        appChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
     }
 }
 
@@ -18,4 +75,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
 }
